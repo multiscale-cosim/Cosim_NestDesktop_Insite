b69e28bd8bebc8701b58540e2341d2b148d5ad2a
mfahdaz <fahadazeemi@gmail.com>
2023-07-18 10:34:34 +0000

    changed bootstrap.sh to install insite from master branch, added functionality to run app server, wait until script is submitted by nest desktop

--- installation/local/bootstrap.sh
+++ installation/local/bootstrap.sh
@@ -213,17 +213,17 @@ cd $CO_SIM_INSITE
 git clone --recurse-submodules https://github.com/VRGroupRWTH/insite.git
 
 cd insite
-git checkout feature/cosim_compat
+git checkout master
 
 mkdir build_access_node
 cd build_access_node
 cmake ../access-node -DBUILD_SHARED_LIBS=OFF
-make -j 3
+make -j 4
 
 cd ..
 mkdir build_nest_module
 cd build_nest_module
 cmake -Dwith-nest=/home/vagrant/multiscale-cosim/nest/bin/nest-config ../nest-module/src -DSPDLOG_INSTALL=ON
-make -j 3 install
+make -j 4 install
 #
 echo "SETUP DONE!"
